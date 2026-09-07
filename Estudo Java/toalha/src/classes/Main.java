62a2ac609f29a2b5e77206c00545745676711d65
Samuel Oliveira <alves.samuca08@gmail.com>
2026-09-07 16:34:37 -0300

    feauture - Criação de Métodos na Classe Toalha e Organização e Utilização dos Métodos na Main

--- Estudo Java/toalha/src/classes/Main.java
+++ Estudo Java/toalha/src/classes/Main.java
@@ -1,7 +1,49 @@
 package classes;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String color;
+        String size;
+        Double wetness;
 
+        System.out.println("Sua Toalha Atributos - DIGITE A COR");
+        color = sc.nextLine();
+
+        System.out.println("Sua Toalha Atributos - DIGITE O TAMANHO (P, M ou G)");
+        size = sc.nextLine();
+
+        Toalha toalha = new Toalha(color, size, 0);
+
+        while(true) {
+            System.out.println("Escolha o que fazer com sua Toalha - Digite o Número da Ação");
+            System.out.println("[1] - Usa-lá pra se Secar");
+            System.out.println("[2] - Torce-lá");
+            System.out.println("[3] - Ver o Maximo de Água que ela Absorve");
+            System.out.println("[4] - Ver se está Seca ou Molhada");
+
+            int acao = sc.nextInt();
+
+            if (acao == 1) {
+                System.out.println("Usar toalha pra se secar digite sua Umidade atual");
+                int amount = sc.nextInt();
+                toalha.Dry(amount);
+            }
+            else if (acao == 2) {
+                toalha.WringOut();
+                System.out.println("Toalha Torcida");
+            }
+            else if (acao == 3) {
+                toalha.getMaxWetness();
+            }
+            else if (acao == 4) {
+                toalha.isDry();
+            }
+            else {
+                System.out.println("Comando Inválido");
+            }
+        }
     }
 }
--- Estudo Java/toalha/src/classes/Toalha.java
+++ Estudo Java/toalha/src/classes/Toalha.java
@@ -1,8 +1,82 @@
 package classes;
 
+import java.awt.*;
+
 public class Toalha {
 
-    public String color;
-    public Double size;
-    public Double wetness;
+    private String color;
+    private String size;
+    private int wetness;
+
+    public Toalha(String color, String size, int wetness) {
+        this.color = color;
+        this.size = size;
+        this.wetness = wetness;
+    }
+
+    public void Dry(int amount) {
+        this.wetness += amount;
+        if (MaxDry(wetness)) {
+            System.out.println("Você se secou");
+        }
+        else {
+            System.out.println("Essa toalha não consegue te secar");
+        }
+    }
+
+    public void WringOut() {
+        this.wetness = 0;
+    }
+
+    public boolean MaxDry(int wetness) {
+        if (this.size == "P" || this.size == "p") {
+            if(wetness <= 50) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        else if (this.size == "M" || this.size == "m") {
+            if(wetness <= 75) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        else if (this.size == "G" || this.size == "g") {
+            if(wetness <= 100) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+        return false;
+    }
+
+    public void getMaxWetness() {
+        if (this.size == "P" || this.size == "p") {
+            System.out.println("Sua toalha absorve até 50 de umidade");
+        }
+
+        else if (this.size == "M" || this.size == "m") {
+            System.out.println("Sua toalha absorve até 75 de umidade");
+        }
+        else if (this.size == "G" || this.size == "g") {
+            System.out.println("Sua toalha absorve até 100 de umidade");
+        }
+    }
+
+    public void isDry () {
+        if (this.wetness == 0) {
+            System.out.println("Toalha Seca");
+        }
+        else {
+            System.out.println("Toalha Molhada");
+        }
+    }
+
 }
